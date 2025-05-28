2aece0d79eb15b192839d86caebbcf1685d92233
kiramman-mp3 <907johan@gmail.com>
2025-05-28 15:10:29 -0500

    Corregí que el footer tapaba la card

--- formulario/solicitud_cambios.php
+++ formulario/solicitud_cambios.php
@@ -7,6 +7,11 @@
     <title>Solicitud de Cambios</title>
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <style>
+        main {
+            padding-bottom: 800px !important;
+        }
+    </style>
 </head>
 
 <body>
